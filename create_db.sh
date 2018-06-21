f8f20aa50f8e98c825431ffad2f0b36d90d0fa9b
Tim See <timothy.w.see@nasa.gov>
2018-06-21 13:29:45 -0400

    added username to create_db.sh script

--- create_db.sh
+++ create_db.sh
@@ -3,19 +3,19 @@
 # databases with the same name to create a new database
 # this is a destructive file i.e. will drop/create DB
 DB=spatial_db
-
+DBUSER=postgres
 mkdb(){
 
 	dbname=${1}
 	isWithPostGIS=${2}
-	
+	user=${3}
 	echo "$0: DROP/CREATE ${dbname}"
 
 	dropdb ${dbname} 2>/dev/null
 	createdb ${dbname} \
 		&& ${isWithPostGIS} && psql ${dbname} -c "create extension postgis;" || true \
-		&& pg_restore --no-acl --no-owner -d ${dbname} < ${dbname}.sql
+		&& pg_restore -U ${user} --no-acl --no-owner -d ${dbname} < ${dbname}.sql
 
 }
-mkdb ${DB} true 
+mkdb ${DB} true ${DBUSER} 
 
